8901610ff59d3b4281e77964fa45da6a2deb791f
chatthong <i.chatthong@gmail.com>
2024-06-18 01:38:36 +0700

    Update index.tsx

--- chat-ui/pages/index.tsx
+++ chat-ui/pages/index.tsx
@@ -9,8 +9,18 @@ export default function IndexPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className={title()}>Make&nbsp;</h1>
+          <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+          <br />
+          <h1 className={title()}>
+            websites regardless of your design experience.
+          </h1>
+          <h4 className={subtitle({ class: "mt-4" })}>
+            Beautiful, fast and modern React UI library.
+          </h4>
 
-         <Card className="max-w-[400px]">
+                   <Card className="max-w-[400px]">
       <CardHeader className="flex gap-3">
         <Image
           alt="nextui logo"
@@ -40,16 +50,7 @@ export default function IndexPage() {
       </CardFooter>
     </Card>
 
-        <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Make&nbsp;</h1>
-          <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-          <br />
-          <h1 className={title()}>
-            websites regardless of your design experience.
-          </h1>
-          <h4 className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library.
-          </h4>
+
         </div>
 
         <div className="flex gap-3">
