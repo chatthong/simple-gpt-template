1436590888be1baa52395934bddff04cb886823d
chatthong <i.chatthong@gmail.com>
2024-06-17 22:37:20 +0700

    Update ChatWindow.js

--- react/src/ChatWindow.js
+++ react/src/ChatWindow.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ChatWindow = () => {
   return (
     <div className="chat-window">
-      <div className="p-3 tabcontent">
+      <div className="p-3">
         <h5>Hello World</h5>
       </div>
     </div>
