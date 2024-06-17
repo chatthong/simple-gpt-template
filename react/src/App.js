ae6991d5281de624bfd836023897e79f497a5658
chatthong <i.chatthong@gmail.com>
2024-06-18 00:02:57 +0700

    ff

--- react/src/App.js
+++ react/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { NextUIProvider, Container } from '@nextui-org/react';
+import { NextUIProvider, Container, Grid, Card, Text, Tabs, Tab } from '@nextui-org/react';
 import ChatContainer from './components/ChatContainer';
 
 const App = () => {
   return (
     <NextUIProvider>
-      <Container display="flex" justify="center" alignItems="center" css={{ height: '100vh' }}>
-        <ChatContainer />
+      <Container css={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Grid.Container gap={2}>
+          <Grid xs={8}>
+            <Card>
+              <Card.Body>
+                <ChatContainer />
+              </Card.Body>
+            </Card>
+          </Grid>
+          <Grid xs={4}>
+            <Card>
+              <Card.Body>
+                <Tabs initialValue="1">
+                  <Tab key="1" title="Chat 1">
+                    <Text>Chat 1 content</Text>
+                  </Tab>
+                  <Tab key="2" title="Chat 2">
+                    <Text>Chat 2 content</Text>
+                  </Tab>
+                  <Tab key="3" title="Chat 3">
+                    <Text>Chat 3 content</Text>
+                  </Tab>
+                </Tabs>
+              </Card.Body>
+            </Card>
+          </Grid>
+        </Grid.Container>
       </Container>
     </NextUIProvider>
   );
