6fa3b1d11f7eef88a2884ee46cfe1e9b96ed6be4
chatthong <i.chatthong@gmail.com>
2024-06-18 01:18:33 +0700

    Update index.tsx

--- chat-ui/pages/index.tsx
+++ chat-ui/pages/index.tsx
@@ -1,54 +1,51 @@
-import { Card, Row, Col, Text, Link, Input, Spacer, Button } from "@nextui-org/react";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import React from "react";
+import { Card, CardHeader, CardBody, CardFooter, Text, Input, Button, Spacer, Container } from "@nextui-org/react";
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      <Row justify="center" align="center" style={{ height: "100vh" }}>
-        {/* Chat Tabs on the left */}
-        <Col span={2} style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-          <Card clickable>
-            <Card.Body>
-              <Text h4>Chat 1</Text>
-            </Card.Body>
-          </Card>
-          <Card clickable>
-            <Card.Body>
-              <Text h4>Chat 2</Text>
-            </Card.Body>
-          </Card>
-          <Card clickable>
-            <Card.Body>
-              <Text h4>Chat 3</Text>
-            </Card.Body>
-          </Card>
-        </Col>
+      <Container>
+        <div className="max-w-full gap-2 grid grid-cols-12 px-8 h-screen">
+          {/* Chat Tabs on the left */}
+          <div className="col-span-2 flex flex-col gap-4">
+            <Card clickable>
+              <CardHeader>
+                <Text h4>Chat 1</Text>
+              </CardHeader>
+            </Card>
+            <Card clickable>
+              <CardHeader>
+                <Text h4>Chat 2</Text>
+              </CardHeader>
+            </Card>
+            <Card clickable>
+              <CardHeader>
+                <Text h4>Chat 3</Text>
+              </CardHeader>
+            </Card>
+          </div>
 
-        {/* Chat Box on the right */}
-        <Col span={10} style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-          <Card>
-            <Card.Header>
-              <Text h3>ChatGPT</Text>
-            </Card.Header>
-            <Card.Body style={{ flex: 1 }}>
-              <Text>This is a chat message.</Text>
-              {/* Add more chat messages here */}
-            </Card.Body>
-            <Card.Footer>
-              <Row align="center">
-                <Col>
+          {/* Chat Box on the right */}
+          <div className="col-span-10 flex flex-col gap-4">
+            <Card className="flex-1">
+              <CardHeader>
+                <Text h3>ChatGPT</Text>
+              </CardHeader>
+              <CardBody>
+                <Text>This is a chat message.</Text>
+                {/* Add more chat messages here */}
+              </CardBody>
+              <CardFooter>
+                <div className="flex w-full gap-2">
                   <Input clearable fullWidth size="lg" placeholder="Type a message" />
-                </Col>
-                <Col auto>
                   <Button auto>Send</Button>
-                </Col>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Col>
-      </Row>
+                </div>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      </Container>
     </DefaultLayout>
   );
 }
