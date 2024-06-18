b4c84247400ae98e84dc42635da1f5461cdd386f
chatthong <i.chatthong@gmail.com>
2024-06-18 10:42:28 +0700

    Update index.tsx

--- chat-ui/pages/index.tsx
+++ chat-ui/pages/index.tsx
@@ -68,25 +68,15 @@ export default function IndexPage() {
                       label="Description"
                       placeholder="Enter your description"
                       className="max-w-xs"
+                      endContent={
+                        <Kbd className="hidden lg:inline-block" keys={["command"]}>
+                          K
+                        </Kbd>
+                      }
+                      startContent={
+                        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+                      }
                     />
-                        <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
                   </CardFooter>
                 </Card>
               </Tab>
