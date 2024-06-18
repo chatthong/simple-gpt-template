6e860020f7ba1a8780e80fb7544523bda5a9414a
chatthong <i.chatthong@gmail.com>
2024-06-18 12:28:37 +0700

    Update index.tsx

--- chat-ui/pages/index.tsx
+++ chat-ui/pages/index.tsx
@@ -92,33 +92,28 @@ export default function IndexPage() {
               </Tab>
 
               <Tab key="chat2" title="Chat #2">
-                <Card className="max-w-[400px]">
-                  <CardHeader className="flex gap-3">
-                    <Image
-                      alt="nextui logo"
-                      height={40}
-                      radius="sm"
-                      src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-                      width={40}
-                    />
-                    <div className="flex flex-col">
-                      <p className="text-md">NextUI</p>
-                      <p className="text-small text-default-500">nextui.org</p>
-                    </div>
-                  </CardHeader>
-                  <Divider />
-                  <CardBody>
-                    <p>Make beautiful websites regardless of your design experience.</p>
-                  </CardBody>
-                  <Divider />
-                  <CardFooter>
-                    <Textarea
-                      label="Description"
-                      placeholder="Enter your description"
-                      className="max-w-xs"
-                    />
-                  </CardFooter>
-                </Card>
+                <Card className="max-w-[340px]">
+                        <CardHeader className="justify-between">
+                          <div className="flex gap-5">
+                            <Avatar isBordered radius="full" size="md" src="https://nextui.org/avatars/avatar-1.png" />
+                            <div className="flex flex-col gap-1 items-start justify-center">
+                              <h4 className="text-small font-semibold leading-none text-default-600">Zoey Lang</h4>
+                              <h5 className="text-small tracking-tight text-default-400">@zoeylang</h5>
+                            </div>
+                          </div>
+                        </CardHeader>
+                        <CardBody className="px-3 py-0 text-small text-default-400">
+                          <p>
+                            Frontend developer and UI/UX enthusiast. Join me on this coding adventure!
+                          </p>
+                          <span className="pt-2">
+                            #FrontendWithZoey 
+                            <span className="py-2" aria-label="computer" role="img">
+                              💻
+                            </span>
+                          </span>
+                        </CardBody>
+                      </Card>
               </Tab>
 
               <Tab key="chat3" title="Chat #3">
