789f24f9e40509c516e4f21cc7ab1afec92e2341
chatthong <i.chatthong@gmail.com>
2024-06-21 22:35:31 +0700

    Update next.config.js

--- chat-ui/next.config.js
+++ chat-ui/next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-   eslint: {
+  eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-}
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    MASTER_PROMPT: process.env.MASTER_PROMPT,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
