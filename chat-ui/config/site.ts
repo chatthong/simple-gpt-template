bc246e28f37ba64ca29e5a3a04d7547f285af52c
chatthong <i.chatthong@gmail.com>
2024-06-25 21:37:29 +0700

    Update site.ts

--- chat-ui/config/site.ts
+++ chat-ui/config/site.ts
@@ -3,10 +3,8 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "Whale GPT",
   description: "Company GPT helper",
-  profileURLAI:
-    "https://scontent.fbkk5-6.fna.fbcdn.net/v/t39.30808-6/338129646_769644241090378_5632671267648388357_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=3M2JQU6FkF0Q7kNvgHYQMwY&_nc_ht=scontent.fbkk5-6.fna&oh=00_AYAUFgFBQXHSEc_yl2iPNyBHNOC9M8hmLwslXjt_FvtvNA&oe=6677A372",
-  profileURLUser:
-    "https://scontent.fbkk5-5.fna.fbcdn.net/v/t39.30808-6/428153685_807892438050041_1223775492341826359_n.png?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=8U_ULHPyzGkQ7kNvgF33F27&_nc_ht=scontent.fbkk5-5.fna&oh=00_AYAQgR7vkahm_-fkEnq5WSSnI7a15sN3gozzLWWUc6YVfw&oe=6677AFB6",
+  profileURLAI: "https://api.dicebear.com/9.x/thumbs/svg?seed=Bella",
+  profileURLUser: "https://api.dicebear.com/9.x/thumbs/svg?seed=Zoey",
   navItems: [
     {
       label: "Home",
